853afd5955a70d053e48f6522bdaf57b0875865f
Chau Batkunda <maniklusi31@gmail.com>
2026-01-20 17:38:54 +0900

    update version v1.0.1

--- stubs/inertia-vue-bootstrap/app/Http/Controllers/Auth/LoginController.php
+++ stubs/inertia-vue-bootstrap/app/Http/Controllers/Auth/LoginController.php
@@ -1,11 +1,22 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     public function create()
     {
         return inertia('Auth/Login');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        return redirect()->route('home');
+
     }
 }
