a42053c829a593f89a947795c9466a930f9fb242
Chau Batkunda <maniklusi31@gmail.com>
2026-01-20 17:51:46 +0900

    v1.0.3

--- src/LarachauServiceProvider.php
+++ src/LarachauServiceProvider.php
@@ -3,8 +3,16 @@
 namespace Larachau\Ui;
 
 use Illuminate\Support\ServiceProvider;
+use Larachau\Ui\Console\InstallCommand;
 
 class LarachauServiceProvider extends ServiceProvider
 {
-
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InstallCommand::class,
+            ]);
+        }
+    }
 }
